574365f64fc171a33dcdcabbd72d752315ec5636
Nicolas <rojonicolasdev@gmail.com>
2023-12-11 16:10:51 -0300

    Added animations

---
+++ app/components/smooth-scroll/index.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+import {ReactLenis, useLenis } from '@studio-freight/react-lenis';
+
+export default function SmoothScroll({children}: any) {
+
+  return (
+    <ReactLenis root>{ children }</ReactLenis>
+  )
+}
