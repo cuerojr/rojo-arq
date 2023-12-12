a7d3a69c9c33d3ff1a11e694defdb5ddd83395a1
Nicolas <rojonicolasdev@gmail.com>
2023-12-12 13:14:55 -0300

    Added animations

--- app/components/mision/index.tsx
+++ app/components/mision/index.tsx
@@ -73,8 +73,8 @@ export default function MisionComponent(): any {
         <div className="text-centered" style={{
           textAlign: 'center',
         }}>
-          <div className={ s.s2 }>Technology &amp; Partners</div>
-          <h2 className={s.text_size_h2}>Top quality solutions</h2>
+          <div className={ s.s2 }>Principios &amp; Fundamentos</div>
+          <h2 className={s.text_size_h2}>Soluciones de alta calidad</h2>
         </div>
       </div>
       { misionData.map((element) => (
@@ -88,12 +88,22 @@ export default function MisionComponent(): any {
                 fontStyle:'italic',
                 marginLeft: '2rem'
               }}>{element.subTitle}</span></h2>              
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="1em"
-                viewBox="0 0 448 512">
-                <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-              </svg>
+              { selectedMision === element.id ? (
+                <svg 
+                  xmlns="http://www.w3.org/2000/svg" 
+                  height="16" 
+                  width="14" 
+                  viewBox="0 0 448 512">
+                  <path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/>
+                </svg>
+              ): (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1em"
+                  viewBox="0 0 448 512">
+                  <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                </svg>                
+              )}
             </div>
           </MisionButtonComponent>
 
