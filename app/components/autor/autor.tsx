b76cd129af85970dd348f27b0aa7a49c25abb678
cuerojr <cannibal5033@gmail.com>
2024-05-24 10:43:16 -0300

    minor fix

---
+++ app/components/autor/autor.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import s from "./style.module.scss";
+import StripesContainer from "../common/stripes";
+
+export default function Auto() {
+  return (
+    <section className="border-1 border-t relative">
+      <div className={s.footer_container}>
+        <StripesContainer />
+        <p
+          className="text-balance font-thin p-3 uppercase"
+          style={{
+            fontSize: "10px",
+          }}
+        >
+          © 2024 rojoarq / Rojo Nicolás
+        </p>
+      </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 'use client'
 
-import Footer from "./components/footer";
-import Header from "./components/header";
-import ServicesList from "./components/services-list";
-import Slogan from "./components/slogan";
-import MisionComponent from "./components/mision";
-import Navbar from './components/navbar';
-import HeroImage from './components/hero-image';
-import Quote from './components/home-quote';
+import Footer from "@/app/components/footer";
+import Header from "@/app/components/header";
+import ServicesList from "@/app/components/services-list";
+import MisionComponent from "@/app/components/mision";
+import Navbar from '@/app/components/navbar';
+import HeroImage from '@/app/components/hero-image';
+import Quote from '@/app/components/home-quote';
 import { Toaster } from '@/components/ui/toaster';
+import Autor from "@/app/components/autor/autor";
 
 export default function Home() {  
   
@@ -20,7 +20,8 @@ export default function Home() {
       <Quote />
       <ServicesList />
       <MisionComponent />
-      <Footer />  
+      <Footer />
+      <Autor />  
       <Toaster />    
     </main>
   )
