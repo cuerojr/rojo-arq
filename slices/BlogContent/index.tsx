1c1eb2d52becccaa19a2ea986f35370dc75556ad
cuerojr <cannibal5033@gmail.com>
2026-03-11 13:10:33 -0300

    asd

--- slices/BlogContent/index.tsx
+++ slices/BlogContent/index.tsx
@@ -20,30 +20,39 @@ const BlogContent: FC<BlogContentProps> = ({ slice }) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="min-h-screen px-44 pt-20"
+      className="max-w-[1440px] mx-auto"
     >
-      <div className="max-h-screen aspect-[9/16]">
-        <PrismicNextImage field={slice.primary.portada} />
+      <div className="relative h-screen overflow-hidden">
+        <div className="absolute inset-0 bg-rojoarq-black opacity-50 w-full h-full object-cover z-10" />
+        <PrismicNextImage
+          field={slice.primary.portada}
+          width={slice.primary.portada.dimensions?.width}
+          height={slice.primary.portada.dimensions?.height}
+          className="h-full w-full object-cover object-center"
+        />
       </div>
-      <div className="text-5xl font-bold text-black">
-        <PrismicRichText field={slice.primary.titulo} />
-      </div>
-      <div className="text-2xl mb-10">
-        <PrismicRichText field={slice.primary.bajada} />
-      </div>
-      <div className="space-y-4">
-        <PrismicRichText field={slice.primary.cuerpo_de_la_noticia} />
-      </div>
-      <div className="  mt-10">
-        <h4 className="text-2xl">Otros posts</h4>
-        <div className="flex gap-4 items-center border-y">
-          {slice.primary.otros_blogs.map((item: any, i: number) => (
-            <div key={item.blog.id} className="py-4">
-              <PrismicNextLink href={item.blog.uid}>
-                {item.blog.uid as string}
-              </PrismicNextLink>
-            </div>
-          ))}
+
+      <div className="min-h-screen p-10 md:px-44 md:pt-20 flex flex-col gap-10 mb-10">
+        <div className="text-5xl font-bold text-black">
+          <PrismicRichText field={slice.primary.titulo} />
+        </div>
+        <div className="text-2xl">
+          <PrismicRichText field={slice.primary.bajada} />
+        </div>
+        <div className="space-y-4">
+          <PrismicRichText field={slice.primary.cuerpo_de_la_noticia} />
+        </div>
+        <div className="">
+          <h4 className="text-2xl">Otros posts</h4>
+          <div className="flex gap-4 items-center border-y">
+            {slice.primary.otros_blogs.map((item: any, i: number) => (
+              <div key={item.blog.id} className="py-4">
+                <PrismicNextLink href={item.blog.uid}>
+                  {item.blog.uid as string}
+                </PrismicNextLink>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
