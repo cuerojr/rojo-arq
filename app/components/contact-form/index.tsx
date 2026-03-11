5f4b6518b72a8aa4fcf4f5e9bd45207388536649
cuerojr <cannibal5033@gmail.com>
2026-03-11 18:40:45 -0300

    kjg

--- app/components/contact-form/index.tsx
+++ app/components/contact-form/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -12,6 +13,7 @@ import {
 import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
 import { useToast } from "@/components/ui/use-toast";
+import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
   nombre: z.string().min(5, {
@@ -63,62 +65,81 @@ export default function ContactForm() {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit((values) => onSubmit(values))}
-        className="space-y-8 w-[100%]"
+        className="flex flex-col justify-center gap-10"
       >
-        <FormField
-          control={form.control}
-          name="nombre"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input placeholder="Nombre" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Textarea placeholder="Mensaje" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <div className="space-y-5">
+          <div className="flex gap-5">
+            <div className="w-1/2">
+              <FormField
+                control={form.control}
+                name="nombre"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="Nombre" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="w-1/2">
+              <FormField
+                control={form.control}
+                name="telefono"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="Teléfono" {...field} />
+                    </FormControl>
 
-        <FormField
-          control={form.control}
-          name="telefono"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input placeholder="Teléfono" {...field} />
-              </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input placeholder="ejemplo@gmail.com" {...field} />
-              </FormControl>
+          <div className="flex gap-5">
+            <div className="w-1/2">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="ejemplo@gmail.com" {...field} />
+                    </FormControl>
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <button type="submit">
-          <span>Enviar</span>
-        </button>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="w-1/2">
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Textarea placeholder="Mensaje" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+        </div>
+        <Button
+          className="cursor-pointer rounded-none w-full py-6"
+          size="lg"
+          variant="default"
+          type="submit"
+        >
+          <span className="text-xl">Enviar</span>
+        </Button>
       </form>
     </Form>
   );
--- app/components/cotizador/cotizador.tsx
+++ app/components/cotizador/cotizador.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import ContactForm from "../contact-form";
 
 function Cotizador() {
@@ -7,10 +8,23 @@ function Cotizador() {
         <span className="px-5">Cotizador</span>
       </div>
 
-      <div className=" fixed inset-0 bg-rojoarq-black/70 z-60 p-20">
-        <div className="bg-rojoarq-arena h-full p-20">
-            <h2>Asesoría</h2>
-            <ContactForm />
+      <div className="min-h-screen fixed inset-0 bg-rojoarq-black/70 z-60 p-20">
+        <div className="bg-rojoarq-white h-full">
+          <div className="flex gap-5 h-full">
+            <div className="w-2/5 relative overflow-hidden">
+              <Image
+                src="/hero-bg.png"
+                alt="Construcciones y reformas en la ciudad de Rosario"
+                height={540}
+                width={960}
+                className="absolute inset-0 w-full h-full object-cover "
+              />
+            </div>
+            <div className="w-3/5 py-15 px-10 ">
+              <h2 className="text-lg underline mb-4">Contactanos</h2>
+              <ContactForm />
+            </div>
+          </div>
         </div>
       </div>
     </>
